dc1f88a8811034a6aa4473d88addcc7c4f397421
dpresnjak <dpresnjak@gmail.com>
2022-06-30 10:26:25 +0200

    Added some code for APIG cdk.

--- cdk_test/cdk_test_stack.py
+++ cdk_test/cdk_test_stack.py
@@ -1,19 +1,40 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
+from asyncio.log import logger
+import aws_cdk as cdk
+import aws_cdk.aws_apigateway as apig
 
-class CdkTestStack(Stack):
+import argparse
+import json
+import logging
+from pprint import pprint
+import boto3
+from botocore.exceptions import ClientError
+import requests
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+logger = logging.getLogger(__name__)
 
-        # The code that defines your stack goes here
+class ApiGateway:
+    def __init__(self, apig_client):
+        self.apig_client = apig_client
+        self.api_id = None
+        self.root_id = None
+        self.stage = None
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkTestQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+    def create_rest_api(self, api_name):
+        try:
+            result = self.apig_client.create_rest_api(name=api_name)
+            self.api_id = result["id"]
+            logger.info(f"Created REST API {api_name} with ID {self.api_id}.")
+        except ClientError:
+            logger.exception(f"Couldn't create REST API {api_name}.")
+            raise
+        return self.api_id
+
+    def create_resource(self, parent_id, resource_path):
+        try:
+            result = self.apig_client.create_resource(
+                restApiId=self.api_id, parentId=parent_id, pathPart=resource_path)
+            resource_id = result["id"]
+            logger.info(f"Created resource {resource_path}.")
+        return resource_id
+
+    def create_method(self, resource_id, rest_method, ):
