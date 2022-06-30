7dc876ffea2625dec527197a129ef6ef85864554
dpresnjak <dpresnjak@gmail.com>
2022-06-30 14:38:20 +0200

    Changed everything, moved over to app.py

--- app.py
+++ app.py
@@ -1,9 +1,93 @@
-#!/usr/bin/env python3
-import os
-import aws_cdk as cdk
-from cdk_test.cdk_test_stack import ApiGateway
-app = cdk.App()
+from constructs import Construct
+from aws_cdk import (App, Stack, aws_lambda as lamb, aws_apigateway as apigw)
 
-ApiGateway(app)
+class ApiGateway(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
+        lambda_upload = lamb.Function(self, "LambdaUploadFunction",
+                                      handler="lambda-upload.handler",
+                                      runtime=lamb.Runtime.PYTHON_3_9,
+                                      code=lamb.Code.from_asset("lambda"))
+
+        lambda_download = lamb.Function(self, "LambdaDownloadFunction",
+                                      handler="lambda-download.handler",
+                                      runtime=lamb.Runtime.PYTHON_3_9,
+                                      code=lamb.Code.from_asset("lambda"))
+
+        base_api = apigw.RestApi(
+            self, "ApiGateway",
+            rest_api_name="ApiGateway",
+            description="An API Gateway with Lambda Integration"
+        )
+
+        integration_options = apigw.IntegrationOptions(
+            connection_type=apigw.ConnectionType("INTERNET"),
+            passthrough_behavior=apigw.PassthroughBehavior("WHEN_NO_MATCH")
+        )
+
+        get_integration = apigw.Integration(
+            type=apigw.IntegrationType("AWS_PROXY"),
+            integration_http_method="POST",
+            # uri=(f"arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/{lambda_download.arn}/invocations"),
+            options=integration_options
+        )
+
+        post_integration = apigw.Integration(
+            type=apigw.IntegrationType("AWS_PROXY"),
+            integration_http_method="POST",
+            # uri=(f"arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/{lambda_upload.arn}/invocations"),
+            options=integration_options
+        )
+
+        method_get = apigw.IResource.add_method(self,
+            http_method="GET",
+            # integration=get_integration,
+            authorization_type="NONE",
+            operation_name="api-get"
+        )
+
+        method_post = apigw.IResource.add_method(self,
+            http_method="POST",
+            # integration=post_integration,
+            authorization_type="NONE",
+            operation_name="api-get"
+        )
+
+        # method_post = apigw.Resource.add_method("POST", api=base_api, http_method="POST")
+
+
+        example_entity = base_api.root.add_resource(
+            'example',
+            default_cors_preflight_options=apigw.CorsOptions(
+                allow_methods=['GET', 'POST'],
+                allow_origins=apigw.Cors.ALL_ORIGINS))
+
+        example_entity_lambda_integration = apigw.LambdaIntegration(
+            lambda_upload,
+            proxy=False,
+            integration_responses=[
+                apigw.IntegrationResponse(
+                    status_code="200",
+                    response_parameters={
+                        'method.response.header.Access-Control-Allow-Origin': "'*'"
+                    }
+                )
+            ]
+        )
+
+        example_entity.add_method(
+            'GET', example_entity_lambda_integration,
+            method_responses=[
+                apigw.MethodResponse(
+                    status_code="200",
+                    response_parameters={
+                        'method.response.header.Access-Control-Allow-Origin': True
+                    }
+                )
+            ]
+        )
+
+app = App()
+ApiGateway(app, "ApiG-stack-cdk")
 app.synth()
--- cdk_test/cdk_test_stack.py
+++ cdk_test/cdk_test_stack.py
@@ -1,27 +1,23 @@
 from asyncio.log import logger
 import aws_cdk as cdk
 import aws_cdk.aws_apigateway as apig
-
-import argparse
-import json
 import logging
-from pprint import pprint
 import boto3
 from botocore.exceptions import ClientError
-import requests
 
 logger = logging.getLogger(__name__)
 
-class ApiGateway:
-    def __init__(self, apig_client):
+class ApiGateway(cdk.Stack):
+    def __init__(self, apig_client, scope: cdk.App) -> None:
         self.apig_client = apig_client
         self.api_id = None
         self.root_id = None
         self.stage = None
 
-    def create_rest_api(self, api_name):
+    def create_rest_api(self, api_name="lambda-api"):
         try:
-            result = self.apig_client.create_rest_api(name=api_name)
+            result = self.apig_client.create_rest_api(name=api_name,
+                                                      description="An API Gateway with Lambda Integration")
             self.api_id = result["id"]
             logger.info(f"Created REST API {api_name} with ID {self.api_id}.")
         except ClientError:
@@ -29,12 +25,70 @@ class ApiGateway:
             raise
         return self.api_id
 
-    def create_resource(self, parent_id, resource_path):
+    def create_resource_post(self, parent_id, resource_path="post"):
         try:
             result = self.apig_client.create_resource(
                 restApiId=self.api_id, parentId=parent_id, pathPart=resource_path)
             resource_id = result["id"]
             logger.info(f"Created resource {resource_path}.")
-        return resource_id
+        except ClientError:
+            logger.exception(f"Couldn't create resource {resource_path}.")
+            raise
+        else:
+            return resource_id
 
-    def create_method(self, resource_id, rest_method, ):
+    def create_resource_get(self, parent_id, resource_path="{filename}"):
+        try:
+            result = self.apig_client.create_resource(
+                restApiId=self.api_id, parentId=parent_id, pathPart=resource_path)
+            resource_id = result["id"]
+            logger.info(f"Created resource {resource_path}.")
+        except ClientError:
+            logger.exception(f"Couldn't create resource {resource_path}.")
+            raise
+        else:
+            return resource_id
+
+    def create_method_post(self, resource_id, service_endpoint_prefix,
+                      service_action, service_method, role_arn, mapping_template, rest_method="POST"):
+
+        service_uri=(f"arn:aws:apigateway:{self.apig_client.meta.region_name}:{service_endpoint_prefix}:action/{service_action}")
+
+        try:
+            self.apig_client.put_method(
+                restApiId=self.api_id,
+                resourceId=resource_id,
+                httpdMethod=rest_method,
+                authorizationType="NONE")
+
+            self.apig_client.put_method_response(
+                restApiId=self.api_id,
+                resourceId=resource_id,
+                httpdMethod=rest_method,
+                statusCode="200",
+                responseModels={"application/json": "Empty"})
+            logger.info(f"Created {rest_method} method for resource {resource_id}.")
+        except ClientError:
+            raise
+
+        try:
+            self.apig_client.put_integration(
+                restApiId=self.api_id,
+                resourceId=resource_id,
+                httpMethod=rest_method,
+                type="AWS_PROXY",
+                passthroughBehaviour="WHEN_NO_MATCH",
+                integrationHttpMethod="POST",
+                uri=(f"arn:aws:apigateway:{self.apig_client.meta.region_name}:lambda:path/2015-03-31/functions/${LambdaFunctionUpload.Arn}/invocations")
+            )
+            self.apig_client.put_integration_response(
+                restApiId=self.api_id,
+                resourceId=resource_id,
+                httpMethod=rest_method,
+                statusCode="200",
+                responseTemplates={"application/json": ""})
+            logger.info(
+                f"Created integration for resource {resource_id} to service URI {service_uri}."
+            )
+        except ClientError:
+            raise
